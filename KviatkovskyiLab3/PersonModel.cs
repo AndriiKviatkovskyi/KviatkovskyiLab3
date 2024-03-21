de80d49f9e72ce0b7752e985eea572154d1ad2b0
AndriiKviatkovskyi <133859288+AndriiKviatkovskyi@users.noreply.github.com>
2024-03-21 16:44:23 +0200

    Add project files.

---
+++ KviatkovskyiLab3/PersonModel.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KviatkovskyiLab3
+{
+    internal class PersonModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string? _email;
+        private DateTime? _birthdate;
+        private int? _age;
+        private readonly bool? _isAdult;
+        private readonly string? _zodiacWestern;
+        private readonly string? _zodiacChinese;
+        private readonly bool? _isBirthday;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PersonModel(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public PersonModel(string firstName, string lastName, string email) : this(firstName, lastName)
+        {
+            _email = email;
+        }
+
+        public PersonModel(string firstName, string lastName, DateTime birthdate) : this(firstName, lastName)
+        {
+            _birthdate = birthdate;
+        }
+
+
+        public PersonModel(string firstName, string lastName, string email, DateTime birthdate) : this(firstName, lastName, email)
+        {
+            _birthdate = birthdate;
+            if (!_birthdate.HasValue) return;
+            _age = CalculateAge();
+            _isAdult = CalculateIsAdult();
+            _isBirthday = CalculateIsBirthday();
+            _zodiacWestern = CalculateZodiacWestern();
+            _zodiacChinese = CalculateZodiacChinese();
+        }
+
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { 
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+                }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string? Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public DateTime? Birthdate
+        {
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+
+        public int? Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public bool? IsAdult
+        {
+            get { return _isAdult; }
+        }
+
+        public string? ZodiacWestern
+        {
+            get { return _zodiacWestern; }
+        }
+
+        public string? ZodiacChinese
+        {
+            get { return _zodiacChinese; }
+        }
+
+        public bool? IsBirthday
+        {
+            get { return _isBirthday; }
+        }
+
+
+        private int? CalculateAge()
+        {
+            DateTime current = DateTime.Now;
+            if (!_birthdate.HasValue) return null;
+            int age = current.Year - _birthdate.Value.Year;
+
+            if (current.Month < _birthdate.Value.Month || (current.Month == _birthdate.Value.Month && current.Day < _birthdate.Value.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        private bool? CalculateIsAdult()
+        {
+            if (!_age.HasValue) return null;
+            if (_age.Value >= 18) return true;
+            return false;
+        }
+
+        private bool? CalculateIsBirthday()
+        {
+            if (!_birthdate.HasValue) return null;
+            DateTime current = DateTime.Now;
+            return (current.Month == _birthdate.Value.Month && current.Day == _birthdate.Value.Day);
+        }
+
+
+
+        private string? CalculateZodiacWestern()
+        {
+            if (!_birthdate.HasValue) return null;
+            DateTime[] zodiacDates = new DateTime[]
+            {
+                new DateTime(_birthdate.Value.Year, 1, 20),      // Aquarius
+                new DateTime(_birthdate.Value.Year, 2, 19),      // Pisces
+                new DateTime(_birthdate.Value.Year, 3, 21),      // Aries
+                new DateTime(_birthdate.Value.Year, 4, 20),      // Taurus
+                new DateTime(_birthdate.Value.Year, 5, 21),      // Gemini
+                new DateTime(_birthdate.Value.Year, 6, 21),      // Cancer
+                new DateTime(_birthdate.Value.Year, 7, 23),      // Leo
+                new DateTime(_birthdate.Value.Year, 8, 23),      // Virgo
+                new DateTime(_birthdate.Value.Year, 9, 23),      // Libra
+                new DateTime(_birthdate.Value.Year, 10, 23),     // Scorpio
+                new DateTime(_birthdate.Value.Year, 11, 22),     // Sagittarius
+                new DateTime(_birthdate.Value.Year, 12, 22),     // Capricorn
+
+            };
+
+            string[] zodiacSigns = { "Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius" };
+
+            for (int i = 0; i < zodiacDates.Length; i++)
+            {
+                if (_birthdate < zodiacDates[i]) return zodiacSigns[i];
+            }
+
+            return zodiacSigns[0];
+        }
+
+        private string? CalculateZodiacChinese()
+        {
+            if (!_birthdate.HasValue) return null;
+
+            int yearInCycle = _birthdate.Value.Year % 12;
+
+            string[] zodiacSigns = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat" };
+
+            return zodiacSigns[yearInCycle];
+        }
+    }
+}
---
+++ KviatkovskyiLab3/PersonViewModel.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace KviatkovskyiLab3
+{
+    internal class PersonViewModel : INotifyPropertyChanged
+    {
+
+        private MainWindow _mainWindow;
+        private PersonModel _person;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand ProceedCommand { get; }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                if (Application.Current.Dispatcher.CheckAccess())
+                {
+                    handler.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                }
+                else
+                {
+                    Application.Current.Dispatcher.Invoke(() => handler.Invoke(this, new PropertyChangedEventArgs(propertyName)));
+                }
+            }
+        }
+
+        public PersonViewModel(MainWindow mainWindow)
+        {
+            _person = new PersonModel("", "");
+            _mainWindow = mainWindow;
+            ProceedCommand = new RelayCommand(Proceed, CanProceed);
+        }
+
+        public string FirstName
+        {
+            get { return _person.FirstName; }
+            set {
+                _person.FirstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return _person.LastName; }
+            set {
+                _person.LastName = value; 
+                OnPropertyChanged(nameof(LastName));
+                }
+
+        }
+
+        public string Email
+        {
+            get { return _person.Email ?? ""; }
+            set { 
+                _person.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public DateTime? Birthdate
+        {
+            get { return _person.Birthdate; }
+            set {
+                _person.Birthdate = value;
+                OnPropertyChanged(nameof(Birthdate));
+            }
+        }
+
+        public string BirthdateString
+        {
+            get { return Birthdate.HasValue ? Birthdate.Value.ToShortDateString() : ""; }
+            set { OnPropertyChanged(nameof(BirthdateString)); }
+        }
+
+        public int Age
+        {
+            get { return _person.Age ?? -1; }
+        }
+
+        public string AgeString
+        {
+            get { return Age == -1 ? "" : Age.ToString(); }
+            set { OnPropertyChanged(nameof(AgeString)); }
+        }
+
+        public bool? IsAdult
+        {
+            get { return _person.IsAdult; }
+        }
+
+        public string AdultString
+        {
+            get { return IsAdult.HasValue ? (IsAdult.Value ? "Adult" : "Child") : ""; }
+            set { OnPropertyChanged(nameof(AdultString)); }
+        }
+
+        public string ZodiacWestern
+        {
+            get { return _person.ZodiacWestern ?? string.Empty; }
+            set { OnPropertyChanged(nameof(ZodiacWestern)); }
+        }
+
+        public string ZodiacChinese
+        {
+            get { return _person.ZodiacChinese ?? string.Empty; }
+            set { OnPropertyChanged(nameof(ZodiacChinese)); }
+        }
+
+        public bool IsBirthday
+        {
+            get { return _person.IsBirthday ?? false; }
+        }
+
+        public string BirthdayString
+        {
+            get { return IsBirthday ? "HAPPY BIRTHDAY!" : ""; }
+            set { OnPropertyChanged(nameof(BirthdayString)); }
+        }
+
+        public PersonModel Person
+        {
+            get { return _person ?? new PersonModel("", ""); }
+            set { _person = value; }
+        }
+
+        private bool CanProceed(object parameter)
+        {
+
+            return !string.IsNullOrWhiteSpace(_mainWindow.EnterFirstName.Text) &&
+                   !string.IsNullOrWhiteSpace(_mainWindow.EnterLastName.Text) &&
+                   !string.IsNullOrWhiteSpace(_mainWindow.EnterEmail.Text) &&
+                   _mainWindow.PickedBirthday.SelectedDate.HasValue;
+        }
+
+        private async void Proceed(object parameter)
+        {
+            foreach (UIElement element in _mainWindow.MainGrid.Children)
+            {
+                element.IsEnabled = false;
+            }
+            _mainWindow.MainWindowName.IsEnabled = false;
+            try
+            {
+                string tempName = _mainWindow.EnterFirstName.Text;
+                string tempSurname = _mainWindow.EnterLastName.Text;
+                string tempEmail = _mainWindow.EnterEmail.Text;
+                DateTime tempDate = DateTime.Now;
+                if (_mainWindow.PickedBirthday.SelectedDate.HasValue)
+                {
+                    tempDate = _mainWindow.PickedBirthday.SelectedDate.Value;
+                }
+                await ProcessAsync(tempName, tempSurname, tempEmail, tempDate);
+
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR");
+                _person = new PersonModel("", "");
+                activateChanges();
+            }
+            foreach (UIElement element in _mainWindow.MainGrid.Children)
+            {
+                element.IsEnabled = true;
+            }
+            _mainWindow.MainWindowName.IsEnabled = true;
+        }
+
+        public async Task ProcessAsync(string tempName, string tempSurname, string tempEmail, DateTime tempDate)
+        {
+            int age = GetAge(tempDate);
+            if (age < 0) throw new NegativeAgeException();
+            if (age > 135) throw new BigAgeException();
+            if (!checkEmail(tempEmail)) throw new InvalidEmailException();
+            await Task.Run(() =>
+            {
+                _person = new PersonModel(tempName, tempSurname, tempEmail, tempDate);
+            });
+            activateChanges();
+        }
+
+        private int GetAge(DateTime date)
+        {
+            DateTime current = DateTime.Now;
+            int age = current.Year - date.Year;
+
+            if (current.Month < date.Month || (current.Month == date.Month && current.Day < date.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+
+        private bool checkEmail(string email)
+        {
+            if (!email.Contains('@')) return false;
+            string[] parts = email.Split('@');
+            if (parts.Length != 2) return false;
+            if (!parts[1].Contains('.')) return false;
+            string[] newParts = parts[1].Split(".");
+            if (newParts.Length != 2) return false;
+            return true;
+        }
+
+        private void activateChanges()
+        {
+            FirstName = FirstName;
+            LastName = LastName;
+            Email = Email;
+            Birthdate = Birthdate;
+            BirthdateString = BirthdateString;
+            AgeString = AgeString;
+            AdultString = AdultString;
+            BirthdayString = BirthdayString;
+            ZodiacWestern = ZodiacWestern;
+            ZodiacChinese = ZodiacChinese;
+        }
+
+
+
+        public class NegativeAgeException : Exception
+        {
+            public NegativeAgeException() : base("Age cannot be under 0.") { }
+        }
+
+        
+        public class BigAgeException : Exception
+        {
+            public BigAgeException() : base("Age cannot be over 135.") { }
+        }
+
+      
+        public class InvalidEmailException : Exception
+        {
+            public InvalidEmailException() : base("Invalid email address.") { }
+        }
+    }
+}
